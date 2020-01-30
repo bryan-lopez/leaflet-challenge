e34ca0debd0bea7b66504182e346bda697944930
Bryan Lopez <bryan.lopez@berkeley.edu>
2020-01-30 15:21:53 -0800

    Adding dependency to config for leaflet

---
+++ static/js/logic.js
@@ -0,0 +1,14 @@
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+
+/* @TODO: Set Up Map via Leaflet and Mapbox
+   @TODO: Set Up Fetch
+   @TODO: Plot USGS Earthquakes
+*/
+
+function makeMap() {
+  
+};
+
+
+
+/* Perform fetch */
