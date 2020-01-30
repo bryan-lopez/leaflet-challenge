0a0d71efb0b96f081b2b60b907d1e1cff5106f7b
Bryan Lopez <bryan.lopez@berkeley.edu>
2020-01-30 15:31:15 -0800

    First Trial

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,10 +5,25 @@ const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significa
    @TODO: Plot USGS Earthquakes
 */
 
-function makeMap() {
-  
+function makeMap(features) {
+  var map = L.map("map").setView([39.828, -98.579], 13)
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    accessToken: API_KEY
+}).addTo(map);
+
+
 };
 
 
 
 /* Perform fetch */
+
+function getFeatures(url) {};
+
+
+/* Function Calls */
+makeMap();
